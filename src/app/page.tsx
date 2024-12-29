b9a7f3290fd260e546197bcb89ba8528e257bdf7
ofcljaved <javedofficialme@gmail.com>
2024-12-29 23:57:32 +0530

    chore: add repo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/input";
 import { UserCard } from "@/components/user-card";
+import { RepoList } from "@/components/repo-list";
 import { fetchUserDetails } from "@/lib/fetchUserDetails";
 import Form from 'next/form';
 
@@ -17,7 +18,13 @@ export default async function Page({
             <Form action="/">
                 <Input name="name" placeholder="Enter your github username" />
             </Form>
-            <UserCard user={user} />
+            {user && <UserCard user={user} />}
+            {user &&
+                <div className="grid md:grid-cols-2 gap-6">
+                    <RepoList title="Original Repositories" repos={user.notForkedRepos} />
+                    <RepoList title="Forked Repositories" repos={user.forkedRepos} forked />
+                </div>
+            }
         </main>
     );
 }
---
+++ src/components/repo-list.tsx
@@ -0,0 +1,30 @@
+import Icon from "./icon";
+
+export const RepoList = ({ title, repos, forked }: { title: string, forked?: boolean, repos: Record<string, any>[] }) => {
+    console.log(repos);
+    return (
+        <div className="space-y-6">
+            <h3 className="text-xl font-semibold flex items-center gap-2 px-2 text-gray-900">
+                <Icon icon={forked ? "gitFork" : "book"} />
+                {title}
+            </h3>
+            {repos.map((repo, i) => (
+                <RepoCard key={i} {...repo} />
+            ))}
+        </div>
+    );
+};
+
+const RepoCard = ({ repo }: { repo: Record<string, any> }) => {
+    return (
+        <div className="flex flex-col items-center gap-3 bg-gray-50 p-4 rounded-lg border border-gray-200">
+            <div className="p-2 bg-blue-50 text-blue-600 rounded-lg">
+                <Icon icon={"book"} />
+            </div>
+            <div>
+                <div className="text-sm text-gray-600">{repo.name}</div>
+                <div className="font-bold text-gray-900">{repo.description}</div>
+            </div>
+        </div>
+    );
+};
