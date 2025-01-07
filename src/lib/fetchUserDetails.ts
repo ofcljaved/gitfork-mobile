bef98b248b00dddfdedfb2d7976ff88d88092528
ofcljaved <javedofficialme@gmail.com>
2025-01-08 01:41:09 +0530

    fix: decrease request time from 4sec to 500ms

--- src/lib/fetchUserDetails.ts
+++ src/lib/fetchUserDetails.ts
@@ -1,3 +1,5 @@
+import { API_URL } from "@/constant";
+
 export interface Repo {
     name: string;
     description: string;
@@ -22,9 +24,9 @@ export interface UserDetails {
 }
 
 export const fetchUserDetails = async (username: string) => {
-    const userUrl = `https://api.github.com/users/${username}`;
-    const reposUrl = `https://api.github.com/users/${username}/repos`;
-    const starsUrl = `https://api.github.com/users/${username}/starred`;
+    const userUrl = `${API_URL}/${username}`;
+    const reposUrl = `${API_URL}/${username}/repos`;
+    const starsUrl = `${API_URL}/${username}/starred?page=1&per_page=1`;
     const perPage = 100;
 
     console.time('fetchUserDetails');
@@ -36,33 +38,38 @@ export const fetchUserDetails = async (username: string) => {
         if (!userResponse.ok) throw new Error('Failed to fetch user data');
         const userData = await userResponse.json();
 
-        let hasMoreRepos = true;
-        let page = 1;
-        let allRepos: Repo[] = [];
-        while (hasMoreRepos) {
-            const reposResponse = await fetch(`${reposUrl}?page=${page}&per_page=${perPage}`, {
+        const starsResponse = await fetch(starsUrl, {
+            cache: 'force-cache',
+        });
+        const linkHeader = starsResponse.headers.get("link");
+        const lastPageLink = linkHeader?.split(",").find((link) => link.includes('rel="last"'));
+        const noOfStars = new URLSearchParams(lastPageLink?.split("?")[1].split(">")[0]).get("page");
+
+        const totalRepoPages = Math.ceil(userData.public_repos / perPage);
+
+        const repoRequests = Array.from({ length: totalRepoPages }, (_, i) =>
+            fetch(`${reposUrl}?page=${i + 1}&per_page=${perPage}`, {
                 cache: 'force-cache',
-            });
-            const reposData = await reposResponse.json();
-            allRepos = allRepos.concat(reposData);
-            hasMoreRepos = reposData.length === perPage;
-            page++;
-        }
+            })
+        );
+
+        const repoResponses = await Promise.all(repoRequests);
+        const allRepos = repoResponses.flat();
+        //
+        ////let hasMoreRepos = true;
+        ////let page = 1;
+        ////let allRepos: Repo[] = [];
+        ////while (hasMoreRepos) {
+        ////    const reposResponse = await fetch(`${reposUrl}?page=${page}&per_page=${perPage}`, {
+        ////        cache: 'force-cache',
+        ////    });
+        ////    const reposData = await reposResponse.json();
+        ////    allRepos = allRepos.concat(reposData);
+        ////    hasMoreRepos = reposData.length === perPage;
+        ////    page++;
+        ////}
         const forkedRepos = allRepos.filter((repo) => repo.fork).sort((a, b) => (new Date(b.updated_at)).getTime() - (new Date(a.updated_at)).getTime());
         const notForkedRepos = allRepos.filter((repo) => !repo.fork).sort((a, b) => (new Date(b.updated_at)).getTime() - (new Date(a.updated_at)).getTime());
-
-        page = 1;
-        let totalStars = 0;
-        let hasMoreStars = true;
-        while (hasMoreStars) {
-            const starsResponse = await fetch(`${starsUrl}?page=${page}&per_page=${perPage}`,{
-                cache: 'force-cache',
-            });
-            const starsData = await starsResponse.json();
-            totalStars += starsData.length;
-            hasMoreStars = starsData.length === perPage;
-            page++;
-        }
 
         const data = {
             username: userData.login,
@@ -71,7 +78,7 @@ export const fetchUserDetails = async (username: string) => {
             bio: userData.bio,
             followers: userData.followers,
             publicRepos: userData.public_repos,
-            totalStars,
+            totalStars:Number(noOfStars || 0),
             forkedRepos,
             notForkedRepos,
         };
