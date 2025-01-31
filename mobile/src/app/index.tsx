49dc74561bb715723fa85eb6aaeeb8243e2571b0
ofcljaved <javedofficialme@gmail.com>
2025-01-31 18:28:38 +0530

    chore: border animation

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,14 +1,13 @@
 import { fetchUser } from "@/actions/fetchUser";
 import { Container } from "@/components/Container";
 import { skipToken, useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBar from "@/components/search-bar";
 import { UserCard } from "@/components/user-card";
 import { RepoList } from "@/components/repo-list";
-import WaveBackground from "@/components/ui/wave-background";
 import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
-import { AntDesign } from "@expo/vector-icons";
-import { Text } from "@/components/ui/text";
+import { Icon } from "@/components/ui/icon";
+import Animated, { interpolate, useAnimatedStyle, useSharedValue, withRepeat, withTiming } from "react-native-reanimated";
 
 export default function Home() {
     const [search, setSearch] = useState<string>("");
@@ -19,21 +18,34 @@ export default function Home() {
     function handleSubmit(value: string) {
         setSearch(value.trim());
     }
+    const offset = useSharedValue<number>(0);
+
+    const animatedStyles = useAnimatedStyle(() => ({
+        width: interpolate(offset.value, [0, 1], [25, 55]),
+        height: interpolate(offset.value, [0, 1], [25, 5]),
+        opacity: interpolate(offset.value, [0, 1], [0.5, 1]),
+    }));
+
+
+    useEffect(() => {
+        offset.value = withRepeat(withTiming(1, { duration: 1750 }), -1, true);
+    }, [])
 
     return (
         <Container className="flex-1">
-            <Text className="text-center text-2xl font-bold">
-                GitFork
-            </Text>
             <Input
                 variant="outline"
                 size="xl"
-                className="rounded-lg"
+                className="rounded-lg relative overflow-auto"
             >
-                <InputSlot>
-                    <AntDesign name="search1" size={20} color="white" />
+                <InputSlot className="w-6 h-6 ml-2">
+                    <Icon className="text-primary-100" name="search" />
                 </InputSlot>
                 <InputField placeholder="Enter Text here..." />
+                <Animated.View
+                    style={[animatedStyles]}
+                    className="absolute w-20 -z-10 rounded-lg rounded-r-[0px] rounded-bl-[0px] border-t border-l -left-[0.5px] -top-[0.5px] border-[#8c98f5]"
+                />
             </Input>
         </Container >
     );
--- mobile/src/components/Container.tsx
+++ mobile/src/components/Container.tsx
@@ -4,8 +4,10 @@ import  Constants  from 'expo-constants';
 
 interface ContainerProps extends ViewProps { }
 
+const STATUS_BAR_MARGIN = Constants.statusBarHeight + 5;
+
 export const Container = ({ children, ...props }: ContainerProps) => {
-    return <SafeAreaView style={{marginTop: Constants.statusBarHeight}} className={cn(
+    return <SafeAreaView style={{ marginTop: STATUS_BAR_MARGIN }} className={cn(
         'flex flex-1 p-6 pb-0',
         props.className
     )}>{children}</SafeAreaView>;
